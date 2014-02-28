788de4d760ab00e6d1074f0f675f3826a3a6c4d5
EgArM <aegorenkov.91@gmail.com>
2014-02-28 12:46:46 +0700

    /globe added

---
+++ globe/index.php
@@ -0,0 +1,4 @@
+<?php
+	header("Location: ../index.php?location=globe");
+	die();
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	if (isset($_GET["lang"])) {
 		include_once "en.php";
+	}elseif (isset($_GET["location"])) {
+		include_once "globe.php";
 	}else
 	{
 		include_once "true_index.php";
